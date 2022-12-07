4cf37b28edfdf773ad1238c6e7955d2265579c4c
ConfusedRaven <lexsmiles@confusedraven.tech>
2022-12-07 20:04:28 +1030

    Hopefully All Saved

---
+++ scenes/game/Overworld.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class Overworld : TileMap
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Vector2i NewSpawnPoint = new Vector2i(32, 1);
+		S
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
