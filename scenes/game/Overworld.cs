f290f4d997b6bb559399988d043ee7e1d6addb0b
ConfusedRaven <lexsmiles@confusedraven.tech>
2023-02-08 20:22:35 +1030

    Got it working on the new computer as well as rider support

--- scenes/game/Overworld.cs
+++ scenes/game/Overworld.cs
@@ -6,9 +6,6 @@ public partial class Overworld : TileMap
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Vector2i NewSpawnPoint = new Vector2i(32, 1);
-		S
-
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
