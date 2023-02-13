85bb73d18891853f0f17a4d280919328ee26dab4
ConfusedRaven <lexsmiles@confusedraven.tech>
2023-02-13 20:26:20 +1030

    Player, Wagon, Movement and most importantly Horse

--- scenes/main.cs
+++ scenes/main.cs
@@ -6,7 +6,7 @@ public partial class main : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Load("res://scenes/game/game.tscn");
+		GD.Load("res://scenes/worlds/game.tscn");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
