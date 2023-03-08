cd4a881e152636948a184de14122a891c4e6e8d3
ConfusedRaven <lexsmiles@confusedraven.tech>
2023-03-08 20:21:02 +1030

    Animations, Menus & Consoles

--- scripts/main_menu.cs
+++ scripts/main_menu.cs
@@ -6,11 +6,12 @@ public partial class main_menu : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		var anim2 = GetNode("Fade").Call("fade_in",1, new Color(0,0,0), "Diamond", false, false);
+		
+		var anim = GetNode<AnimationPlayer>("AnimationPlayer");
+		anim.Play("animate_ui");
 	}
-
-
-	private void _on_timer_timeout()
+	private void _on_button_button_down()
 	{
 		GetTree().ChangeSceneToFile("res://scenes/worlds/game.tscn");
 	}
