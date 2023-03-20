8c123d9976e38cd300c39d752b5670045ffe1ac6
ConfusedRaven <lexsmiles@confusedraven.tech>
2023-03-20 20:18:48 +1030

    shaders

--- scripts/main_menu.cs
+++ scripts/main_menu.cs
@@ -6,8 +6,6 @@ public partial class main_menu : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var anim2 = GetNode("Fade").Call("fade_in",1, new Color(0,0,0), "Diamond", false, false);
-		
 		var anim = GetNode<AnimationPlayer>("AnimationPlayer");
 		anim.Play("animate_ui");
 	}
