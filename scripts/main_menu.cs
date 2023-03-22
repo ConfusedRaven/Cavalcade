84822b8f38b69663a1e12d2dc9b218a1a259632c
ConfusedRaven <lexsmiles@confusedraven.tech>
2023-03-22 20:13:32 +1030

    nothign

--- scripts/main_menu.cs
+++ scripts/main_menu.cs
@@ -6,6 +6,8 @@ public partial class main_menu : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		var anim2 = GetNode<AnimationPlayer>("AnimationPlayer2");
+		anim2.Play("fade");
 		var anim = GetNode<AnimationPlayer>("AnimationPlayer");
 		anim.Play("animate_ui");
 	}
